60bd0d99a4fb223351f1a69bc7dfbb9a0aef3e0d
yoann <yoann.chambeux@hotmail.com>
2024-06-26 10:03:31 +0200

    correction sur le chemin des assets

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'assets': path.resolve(__dirname, './src/assets')
+    }
+  },
   server: {
     open: true
-  },
-  build: {
-    outDir: 'dist'
   }
 });
